1e6f394104d1c7ec04fe8ace2946becb19e84a5b
sakibrimon <mdsakibhassanrimon@gmail.com>
2024-10-24 09:23:51 +0600

    made tags responsive

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -25,13 +25,15 @@ const BookDetails = () => {
                 <p className="mt-5 text-[#131313CC] font-medium text-xl">By : {book.author}</p>
                 <p className="mt-6 py-4 border-y text-[#131313CC] font-medium text-xl">{book.category}</p>
                 <p className="my-6 text-[#131313B3]"><span className="font-bold text-[#131313]">Review :</span> {book.review}</p>
-                <div className="mt-auto">
+                <div className="mt-auto flex items-center">
                     <span className="font-bold text-[#131313]">Tag</span>
+                    <div className="flex flex-wrap gap-3">
                     {
                         book.tags.map((tag, idx) => <button
                             key={idx}
                             className="ml-3 btn btn-xs text-prime bg-[#23BE0A0D] rounded-[30px]">#{tag}</button>)
                     }
+                    </div>
                 </div>
                 <div className="mt-6 pt-6 border-t">
                     <div className="grid grid-cols-2 gap-x-[60px]">
